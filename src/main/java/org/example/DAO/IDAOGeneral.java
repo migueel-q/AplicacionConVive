fcebfedbe8a8524e0a48f4468956a3d02ee076dc
Rebeca Cabo <rebecacaboo14@gmail.com>
2025-12-16 18:00:45 +0100

    Creadas las clases tareaDao y gastoDao completadas con los metodos

---
+++ src/main/java/org/example/DAO/IDAOGeneral.java
@@ -0,0 +1,11 @@
+package org.example.DAO;
+
+import java.util.List;
+
+public interface IDAOGeneral<T, Integer> {
+    public void   insertar(T t);
+    public void   actualizar(T t);
+    public void  eliminar(Integer id);
+    public List<T> obtenerTodos();
+    public T obtenerPorId(Integer id);
+}
