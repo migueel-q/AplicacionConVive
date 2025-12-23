c9dc052ad60468c347029185e1e64397a3e189f5
Miguel Ángel Quian Díaz <mquiand2401@educantabria.es>
2025-12-23 16:07:47 +0100

    controladores y clases modelos. falta PisoControlador de manu

--- WinFormsAppProyecto/Modelos/Contrato.cs
+++ WinFormsAppProyecto/Modelos/Contrato.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace Modelos
 {
     public class Contrato
     {
+        private int id {  get; set; }
+        private String descripcion {  get; set; }
+        private Double precio { get; set; }
+        private DateOnly fechaInicio { get; set; }
+        private DateOnly fechaFin {  get; set; }
+        private List<Inquilino> inquilinos { get; set; }
+        private int propietarioId { get; set; } 
+        private int pisoId { get; set; }
     }
 }
--- WinFormsAppProyecto/Modelos/Piso.cs
+++ WinFormsAppProyecto/Modelos/Piso.cs
@@ -8,5 +8,15 @@ namespace Modelos
 {
     public class Piso
     {
+        private int id {  get; set; }
+        private string direccion {  get; set; }
+        private string descripcion { get; set; }
+        private string imagenUrl { get; set; }
+        private bool disponible { get; set; }
+        private List<Inquilino> inquilinos { get; set; }
+        private int propietarioId { get; set; }
+        private List<Oferta> ofertas { get; set; }
+        private List<Gasto> gastos { get; set; }
+        private List<Contrato> contratos { get; set; }
     }
 }
